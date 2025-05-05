0328c43861b58f37b712107014b60a2dc37dee4a
alek628 <dordzhiev.av@phystech.edu>
2025-05-05 19:31:05 +0300

    changed models, added modified model variants (w/o ids) for input

--- src/models.py
+++ src/models.py
@@ -1,25 +1,40 @@
-from typing import List
-from pydantic import BaseModel
+from typing import List, Optional
+from pydantic import BaseModel, SecretStr
 
 
-class Experiment(BaseModel):
-    id: int | None
+class BaseExperiment(BaseModel):
     name: str
+    description: str
     state: str
-    researcher: str
-    tags: List[str]
+    assignee: Optional[int] = None
+    tags: Optional[List[str]] = None
+
+
+class Experiment(BaseExperiment):
+    experiment_id: int
+
+
+class InputExperiment(BaseExperiment): ...
 
 
 class State(BaseModel):
-    id: int
+    state_id: int
     name: str
 
 
-class Researcher(BaseModel):
-    id: int
+class BaseUser(BaseModel):
     name: str
+    email: str
+    password: SecretStr
+
+
+class User(BaseUser):
+    user_id: int
+
+
+class InputUser(BaseUser): ...
 
 
 class Tag(BaseModel):
-    id: int
+    tag_id: int
     name: str
